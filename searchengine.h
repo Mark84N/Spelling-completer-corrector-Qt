e5f63b67b0f7310f385094b175496ac38781602d
Dmitriy <dieiev.dima@gmail.com>
2016-09-02 15:43:55 +0300

    little documentation
    wrote little documentation, set const argument in
    void known(const QVector<QString> &result,
    DictCandidates& candidates);
    because result isn't actually modified there

--- searchengine.h
+++ searchengine.h
@@ -1,3 +1,26 @@
+/*
+    Current class is responsible for working with text source (vast text file) and has powerful mechanism of treating
+    misspellings and finding correct words. The work is heavily dependent on Dictionary and Hash table features.
+
+    QHash<string, int> is the main dictionary, where word is a key, frequency of use as per .txt file - is a value. It has
+    robust finding capabilities.
+
+    std::map<int, string> is the intermediate dictionary, used during the process of finding matches.
+    Frequency of use of the word (as per vast text file) is a key, word  itself is a value. Here are stored the search results that were found in main dictionary (QHash), sorted  by their so-called index of frequency of use - i.e. how many times word has been
+    used in the source file. That's why source file has to contain plenty of different-style novels and, possible, articles.
+
+    Algorithm of work:
+
+    - it receives the word that user was trying to type
+    - then searches for a full match according to existing dictionary
+    - if there's no full match, then word is being altered in 4 possible ways and the process of finding matches is continued for
+        every altered word
+    - in case of still no matches, previous list of altered words is being altered once again, and the process of finding
+        is continued
+    - if no matches at all - the original word is returned.
+
+*/
+
 #ifndef SEARCHENGINE_H
 #define SEARCHENGINE_H
 
@@ -10,9 +33,8 @@
 #include <QStringList>
 #include <QMessageBox>
 
-//! [0]
-namespace SE{
-
+namespace SE
+{
     using Dictionary = QHash <QString, int>;
     /* std::map is needed for default sorting and reverse iterators */
     using DictCandidates = std::map <int, QString>;
@@ -26,7 +48,7 @@ namespace SE{
         void edits(const QString& word,
                        QVector<QString>& result);
         /* Checks if altered word is present in dictionary */
-        void known(QVector<QString>& result,
+        void known(const QVector<QString> &result,
                        DictCandidates& candidates);
 
     public:
@@ -35,6 +57,5 @@ namespace SE{
         QStringList correct(const QString& word);
     };
 }
-//! [0]
 
 #endif // SEARCHENGINE_H
